a9a07d53e1e4e6477b754015858f18d15de0b6fb
Kyoka Miyamura <kyokamiya@Kyokas-MacBook-Air.local>
2024-07-16 23:51:07 +0900

    fix github action's yml file

--- src/main/java/com/example/account/repository/UserRepository.java
+++ src/main/java/com/example/account/repository/UserRepository.java
@@ -2,6 +2,13 @@ package com.example.account.repository;
 
 import com.example.account.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByNickname(String nickname);
+    @Query("SELECT u FROM User u WHERE u.user_id = :userId")
+    Optional<User> findByUserId(@Param("userId") String userId);
 }
